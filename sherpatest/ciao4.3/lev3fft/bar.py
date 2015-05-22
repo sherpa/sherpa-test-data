48c7bf161dff1d7ad202acb25d56d63689430559
Omar Laurino <olaurino@cfa.harvard.edu>
2015-05-22 13:45:55 -0400

    remove dependency on data model from test

--- sherpatest/ciao4.3/lev3fft/bar.py
+++ sherpatest/ciao4.3/lev3fft/bar.py
@@ -4,7 +4,7 @@ from sherpa.astro.ui import *
 
 image_file = "acisf07999_000N001_r0035_regevt3_srcimg.fits"
 psf_file = "acisf07999_000N001_r0035b_psf3.fits"
-reg_file = "acisf07999_000N001_r0035_reg3.fits"
+reg_file = "ellipse(3145.8947368421,4520.7894736842,37.0615234375,15.3881587982,92.2273254395)"
 srcid = 1
 
 load_data(srcid, image_file)
@@ -12,7 +12,7 @@ load_psf("psf%i" % srcid, psf_file)
 set_psf(srcid, "psf%i" % srcid)
 
 set_coord(srcid, "physical")
-notice2d_id(srcid, "region(" + reg_file + "[SRCREG])")
+notice2d_id(srcid, reg_file)
 
 # Switch to WCS for fitting
 set_coord(srcid, "wcs")
@@ -26,7 +26,7 @@ guess(srcid, src)
 
 image_file = "acisf08478_000N001_r0043_regevt3_srcimg.fits"
 psf_file = "acisf08478_000N001_r0043b_psf3.fits"
-reg_file = "acisf08478_000N001_r0043_reg3.fits"
+reg_file = "ellipse(3144.5238095238,4518.8095238095,25.2978591919,19.1118583679,42.9872131348)"
 srcid = 2
 
 load_data(srcid, image_file)
@@ -34,7 +34,7 @@ load_psf("psf%i" % srcid, psf_file)
 set_psf(srcid, "psf%i" % srcid)
 
 set_coord(srcid, "physical")
-notice2d_id(srcid, "region(" + reg_file + "[SRCREG])")
+notice2d_id(srcid, reg_file)
 
 # Switch to WCS for fitting
 set_coord(srcid, "wcs")
