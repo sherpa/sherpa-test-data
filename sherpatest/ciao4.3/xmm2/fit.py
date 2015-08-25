853fe8ddbaf1fa338a9455e18400ec5ed8c7ed6c
Omar Laurino <olaurino@cfa.harvard.edu>
2015-08-25 16:17:15 -0400

    make sure xmm2 thread cleans after itself

--- sherpatest/ciao4.3/xmm2/fit.py
+++ sherpatest/ciao4.3/xmm2/fit.py
@@ -3,6 +3,7 @@ from sherpa.astro.ui import *
 load_data("MNLup_2138_0670580101_EMOS1_S001_spec.15grp")
 
 #ignore_bad()
+old_abund = get_xsabund()
 set_analysis('energy')
 ignore(None, .2)
 ignore(5., None)
@@ -12,3 +13,4 @@ set_model(1, xsapec.a1)
 a1.kT  = 1
 a1.norm = 1e-4
 subtract(1)
+set_xsabund(old_abund)
