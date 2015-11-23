3b4b6a3cce0dce669bd3db1f6f1801883b524463
Jamie Budynkiewicz <jbudynkiewicz@cfa.harvard.edu>
2015-11-23 12:47:51 -0500

    update load_template* scripts with new bb_data.dat filenames

--- sherpatest/load_template_interpolator-fit.py
+++ sherpatest/load_template_interpolator-fit.py
@@ -3,7 +3,7 @@
 import sherpa.ui as ui
 from sherpa.models.template import KNNInterpolator
 
-ui.load_data("custom_interp", "bb_data.dat")
+ui.load_data("custom_interp", "load_template_interpolator-bb_data.dat")
 ui.load_template_interpolator('knn', KNNInterpolator, k=2, order=1)
 ui.load_template_model('bb1', "bb_index.dat", template_interpolator_name='knn')
 ui.set_model("custom_interp", "bb1")
--- sherpatest/load_template_without_interpolation-check_discrete.py
+++ sherpatest/load_template_without_interpolation-check_discrete.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import sherpa.ui as ui
 
-ui.load_data("bb_data.dat")
+ui.load_data("load_template_without_interpolation-bb_data.dat")
 ui.load_template_model('bb1', "bb_index.dat", template_interpolator_name=None)
 ui.set_source('bb1')
 assert ui.get_source().is_discrete
