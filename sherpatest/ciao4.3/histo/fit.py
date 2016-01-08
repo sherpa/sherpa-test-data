b06d88d1a4cb61f579976f880710ad8a6ca41fc2
Omar Laurino <olaurino@cfa.harvard.edu>
2016-01-08 10:42:46 -0500

    fix #113 and support numpy 1.10

--- sherpatest/ciao4.3/histo/fit.py
+++ sherpatest/ciao4.3/histo/fit.py
@@ -2,12 +2,10 @@ from sherpa.astro.ui import *
 import numpy
 load_data(1, 'cstat.dat', colkeys=('zabs1.nh', 'p1.gamma', 'p1.ampl'))
 
-x=None; y=None
-# Can compare numpy version strings this way
-if (numpy.version.version < "1.3.0"):
-    y, x = numpy.histogram(get_data().y, bins=50, normed=True, new=True)
-else:
-    y, x = numpy.histogram(get_data().y, bins=50, normed=True)
+x = None
+y = None
+# The following line won't work with numpy < 1.3.0, but we do not support this version anymore
+y, x = numpy.histogram(get_data().y, bins=50, normed=True)
 x = x[:-1]
 load_arrays(1, x, y)
 set_model(1, gauss1d.g1)
