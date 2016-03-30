9475f88875ae09cbb2ad615bd32a6836b2223ffb
Omar Laurino <olaurino@cfa.harvard.edu>
2016-03-30 16:01:39 -0400

    fix setup.py data files

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import versioneer
 
 package_name = 'sherpatest'
 
-files = []
-for root, dirnames, filenames in os.walk(package_name):
-    for filename in filenames:
-        files.append(os.path.join(root, filename))
+files = [f for f in os.listdir(package_name)]
+#for root, dirnames, filenames in os.walk(package_name):
+#    for filename in filenames:
+#        files.append(os.path.join(root, filename))
 
 versioneer.VCS = 'git'
 versioneer.versionfile_source = '{}/_version.py'.format(package_name)
@@ -18,7 +18,7 @@ versioneer.parentdir_prefix = '{}-'.format(package_name)
 setup(name=package_name,
       version=versioneer.get_version(),
       packages=[package_name],
-      package_data={'sherpatest': files},
+      package_data={package_name: files},
       include_package_data=True,
       cmdclass=versioneer.get_cmdclass(),
       )
